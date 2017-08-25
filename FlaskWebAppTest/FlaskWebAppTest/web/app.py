472323518a3396844a3f2cc05c12ba4107405a24
R <rumia_cirno@outlook.com>
2017-08-25 17:34:24 +0800

    Init

---
+++ FlaskWebAppTest/FlaskWebAppTest/web/app.py
@@ -0,0 +1,29 @@
+import logging
+import asyncio
+import os
+import json
+import time
+import datetime
+from aiohttp import web
+
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+
+def index(request):
+    return web.Response(body=b"<h1>Awesome</h1>",content_type="text/html")
+
+
+
+async def init(loop):
+    app=web.Application(loop=loop)
+    app.router.add_route("GET","/",index)
+    srv=await loop.create_server(app.make_handler(),"127.0.0.1",9999)
+    logging.info("server stared...")
+    return srv
+
+loop=asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
